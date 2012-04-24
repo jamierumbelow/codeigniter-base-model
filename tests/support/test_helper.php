8a175e3bf0ce805e0d176494b590248ea490f52a
Jamie Rumbelow <jamie@jamierumbelow.net>
2012-04-24 23:07:45 +0100

    Reshuffled the exceptions

--- tests/support/test_helper.php
+++ tests/support/test_helper.php
@@ -56,14 +56,21 @@ function plural($name)
  * Let our tests know about our callbacks
  */
 
-class Callback_Test_Exception extends Exception
+class MY_Model_Test_Exception extends Exception
 {
     public $passed_object = FALSE;
 
+    public function __construct($passed_object, $message = '')
+    {
+        parent::__construct($message);
+        $this->passed_object = $passed_object;
+    }
+}
+
+class Callback_Test_Exception extends MY_Model_Test_Exception
+{
     public function __construct($passed_object)
     {
-        parent::__construct('Callback is being successfully thrown');
-
-        $this->passed_object = $passed_object;
+        parent::__construct($passed_object, 'Callback is being successfully thrown');
     }
 }
