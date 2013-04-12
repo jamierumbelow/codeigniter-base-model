713328a0b15b0d0b540512a292563c39ca38c7fa
Colby Sollars <colby@nucleartuxedo.com>
2013-04-12 12:23:37 -0700

    Added form validation class so tests will pass

--- tests/support/test_helper.php
+++ tests/support/test_helper.php
@@ -16,6 +16,7 @@
 require_once 'vendor/autoload.php';
 
 require_once 'tests/support/database.php';
+require_once 'tests/support/validation.php';
 
 
 /**
---
+++ tests/support/validation.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * A mockery of CI's CI_Form_Validation class
+ */
+
+class Mock_Form_Validation
+{
+	public function set_rules() {}
+	public function set_message() {}
+	public function set_error_delimiters() {}
+	public function error() {}
+	public function error_string() {}
+	public function run() {}
+}
+
+?>
