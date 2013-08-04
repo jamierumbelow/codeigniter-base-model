f11f81c8c8d02372707b6b0833ee9d1cffa06ed2
Ronnie Skansing <Rskansing@gmail.com>
2013-08-04 19:21:05 +0200

    Updated MY_Model.php - Fixed broken validation prep.
    Set the validate function to return $_POST instead of data, as the prepped data is transfered back to $_POST.
    
    Unless we do this, something like 
           <script>alert('ola')</script>
    is passing into the db even with a rule like 'trim|strip_tags'

--- core/MY_Model.php
+++ core/MY_Model.php
@@ -838,7 +838,7 @@ class MY_Model extends CI_Model
 
                 if ($this->form_validation->run() === TRUE)
                 {
-                    return $data;
+                    return $_POST;
                 }
                 else
                 {
@@ -849,7 +849,7 @@ class MY_Model extends CI_Model
             {
                 if ($this->form_validation->run($this->validate) === TRUE)
                 {
-                    return $data;
+                    return $_POST;
                 }
                 else
                 {
