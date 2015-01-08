f2d7ac3bae9b136f725c10f6baa9f4f80f418fc3
Ariel Vallese <alariva@gmail.com>
2015-01-08 14:19:29 -0500

    Using old fashioned array declaration syntax

--- core/MY_Model.php
+++ core/MY_Model.php
@@ -257,7 +257,7 @@ class MY_Model extends CI_Model
 
         if ($data !== FALSE)
         {
-            if(!is_array($primary_value)) $primary_value = [$this->primary_key => $primary_value];
+            if(!is_array($primary_value)) $primary_value = array($this->primary_key => $primary_value);
             
             $result = $this->_database->where($this->primary_key, $primary_value)
                                ->set($data)
